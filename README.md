0a4a3eba8146fd72a650e2d6e812385edad07032
Whasajiggawho <Whoosajiggawha@gmail.com>
2020-07-28 21:08:19 -0400

    Added ability to exclude certain recipes

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ Pass one or more item IDs as parameters. For example, `http://www.eqtraders.com/
 You will get a list back of all of the components required for all of the combines used by that item. Doesn't recurse through subcombines or anything. Just gets all of the recipes that use a particular item and adds up all of their components.
 
 For items that are used to make an item that has a lot of different recipes you might gety wonky results. For example, Celestial Solvent to make Celestial Essence.
+
+### New in version 1.1:
+You can now pass an **optional** file at the end of the list of IDs. This file should include a list of recipes you already know, and they will be potentially excluded from the output. To generate this file log in to your character and run `/outputfile recipes <tradeskill>`. Run `/outputfile recipes` for a list of possible `<tradeskill>` options.
+
+This does take some extra time for larger files because it has to cross-reference to a master list of all possible recipes to match the EQTC ID with the actual EQ recipe. Be patient.
+
+```
+Examples:
+	List components of all recipes using Fine Parchment:
+		java -jar EQRecipeThing.jar 20916
+	List components of all recipes using Fine Parchment that you don't already know:
+		java -jar EQRecipeThing.jar 20916 C:/EverQuest/YourName_cazic-Resedarch-Recipes.txt
+	List components of all recipes using Fine Parchment and all recipes using Celestial Solvent: 
+		java -jar EQRecipeThing.jar 20916 1618
+```
+
+Excluded recipes are **not perfect**. This was originally intended to be used for Spell Research, and it mostly works fine for that. Issues arise when there are multiple recipes that generate the same item. Because of how EQTC stores **items** and not **recipes** it is difficult to tell if you know a particular recipe for a given item just because you know a recipe that produces that item. Read the output carefully to understand if you might need to look something up manually afterwards. I might eventually get around to making it smarter, but for now I'm lazy.
+
+It's also important to note that not all of the recipes this tool finds for you are guaranteed to count towards 350 in a given skill. Again, laziness. I'll get around to making it smarter later, maybe.
--- src/main/groovy/whasa/eq/ts/EQRecipeThing.groovy
+++ src/main/groovy/whasa/eq/ts/EQRecipeThing.groovy
@@ -4,12 +4,47 @@ import org.cyberneko.html.parsers.SAXParser
 
 class EQRecipeThing {
 	public static void main(String[] args) {
-		if(!validate(args)) {
+		if (!validate(args)) {
 			printUsage()
 			System.exit(1)
 		}
 
-		for(String id : args) {
+		List<String> ids = []
+		def exclude = []
+		def master = []
+
+
+		if (args.size() == 1) ids = args
+		else {
+			ids.addAll(args[0..args.size() -2])
+			if(args.last().isInteger()) ids.add(args.last())
+			else {
+				println """\
+			================================================================================
+			===== EXCLUDING ALL KNOWN RECIPES FROM [${new File(args.last()).getCanonicalPath()}]
+			================================================================================""".stripIndent()
+				getClass().getResource('/master.txt').eachLine { line ->
+					master.add (
+							[
+									name: 		line.split(/\t/)[0].replace('"', ''),
+									trivial: 	line.split(/\t/)[1].replace('"', ''),
+									eqtc: 		line.split(/\t/)[2].replace('"', ''),
+									zam: 		line.split(/\t/)[3].replace('"', ''),
+									eq: 		line.split(/\t/)[4].replace('"', ''),
+									expansion: 	line.split(/\t/)[5].replace('"', ''),
+							]
+					)
+
+				}
+				new File(args.last()).readLines().each { String line ->
+					exclude.addAll(master.findAll { it.eq == line.split(/\t/)[0] })
+				}
+			}
+		}
+
+
+		for(String id : ids) {
+			def recheck = []
 			println """\
 			================================================================================
 			===== LOCATING RECIPES FOR ITEM ID [${id}]
@@ -18,13 +53,36 @@ class EQRecipeThing {
 			int count = 0
 			while(true) {
 				String url = "http://www.eqtraders.com/search/reverse_recipe_search.php?item=${id}&min=${count}"
-				println url
+				println "Checking for recipes ${count} - ${count + 25}"
 				def parser = new SAXParser()
 				def page = new XmlSlurper(parser).parse(url)
 				def recipes = page.depthFirst().findAll {
-					!it.children().findAll {
+					def good = !it.children().findAll {
 						it.@href =~ /^\/items\/show_item\.php\?item=\d+$/
 					}.isEmpty()
+					if(good)
+					{
+						def siblings = it.parent().children().list()
+						def index = siblings.indexOf(it)
+						def sibling = siblings[index - 1]
+						String href = sibling.depthFirst().find { it.@href =~ /^\/items\/show_item\.php\?item=\d+&menustr=\d+$/ }.@href
+						String eqtcId = href.substring(href.indexOf('item=') + 5)
+						eqtcId = eqtcId.substring(0, eqtcId.indexOf('&'))
+						if(exclude.find { it.eqtc == eqtcId }) {
+							def masterExcluded = master.findAll { it.eqtc == eqtcId }.sort()
+							def currentExcluded = exclude.findAll { it.eqtc == eqtcId }.sort()
+							println ("Excluding recipes that generate EQTC item ${eqtcId}")
+							if(masterExcluded == currentExcluded) {
+								println '\tYou know every recipe that generates that item.'
+							} else {
+								println '\tYou don\'t know every recipe that generates that item. You should check it yourself manually'
+								println "\thttp://www.eqtraders.com/items/show_item.php?item=${eqtcId}"
+								recheck.addAll(eqtcId)
+							}
+							good = false
+						}
+					}
+					return good
 				}
 
 				if(recipes.isEmpty()) {
@@ -48,37 +106,60 @@ class EQRecipeThing {
 				}
 				count+=25
 			}
+
+			/*
+			 * TODO: Maybe in the future go and check the list of excluded recipes against Allakhazam
+			 *  EQTC doesn't store things in terms of RECIPES. They store things in terms of ITEMS that a recipe produces
+			 * 	So I can't know for sure that you know a recipe because you know the recipe ID,
+			 * 	because there might be two recipes that generate the same EQTC item.
+			 * 	Right now I'm just saying "go and check yourself" and ignoring it. I could eventually go and check on
+			 * 	Allakhazam, for you, but I'm lazy.
+			 */
+//			println """\
+//			================================================================================
+//			===== Rechecking excluded items for possible multiple recipes
+//			================================================================================""".stripIndent()
+//			recheck.each { eqtcId ->
+//			}
+
 			println """\
 			================================================================================
 			===== ITEMS REQUIRED FOR ITEM ID [${id}] RECIPES:
+			===== IMPORTANT: NOT ALL RECIPES ARE GUARANTEED TO COUNT TOWARDS 350 SKILL
 			================================================================================""".stripIndent()
 			def keys = items.keySet().sort()
 			keys.each {
 				println "${it}: ${items[it]}"
 			}
 		}
-
-
-
-
-		//www.eqtraders.com/search/reverse_recipe_search.php?item=20916
-		//http://www.eqtraders.com/search/reverse_recipe_search.php?item=20916&min=25
-
 	}
 
 	static boolean validate(String[] args) {
-		for(String s : args) {
-			try {
-				s.toInteger()
-			} catch(Exception ignored) {
-				return false
+		if(args.size() == 0) return false
+		if(args.size() == 1) return args[0].isInteger()
+		for(int i = 0; i < args.size(); i++)
+		{
+			String s = args[i]
+			if(!s.isInteger())
+			{
+				if(i != args.size() - 1) return false
+				if(!new File(s).exists()) return false
 			}
 		}
-		return args.size() > 0
+		return true
 	}
 
 	static void printUsage() {
 		System.err.println("""\
-		You dun goofed.""".stripIndent())
+You dun goofed.
+Provide a list of EQTC Item IDs followed by an optional `/outputfile recipes <tradeskill>` file of recipes to exclude.
+If the optional `/outputfile` file is provided the file must exist.
+Examples:
+	List components of all recipes using Fine Parchment:
+		java -jar EQRecipeThing.jar 20916
+	List components of all recipes using Fine Parchment that you don't already know:
+		java -jar EQRecipeThing.jar 20916 C:/EverQuest/YourName_cazic-Resedarch-Recipes.txt
+	List components of all recipes using Fine Parchment and all recipes using Celestial Solvent: 
+		java -jar EQRecipeThing.jar 20916 1618""")
 	}
 }
